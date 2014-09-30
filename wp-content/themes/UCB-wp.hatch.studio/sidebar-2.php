0e333c3da0c82f4b29d1a7511456137d9d654bfb
Yuca Suzuki <beru0891@yahoo.co.jp>
2014-09-29 21:10:29 -0700

    カテゴリのIDの順番が間違えていたので修正

--- wp-content/themes/UCB-wp.hatch.studio/sidebar-2.php
+++ wp-content/themes/UCB-wp.hatch.studio/sidebar-2.php
@@ -6,8 +6,8 @@
         <li><a href="<?php echo get_category_link('2'); ?>">文系学部生</a></li>
         <li><a href="<?php echo get_category_link('3'); ?>">理系学部生</a></li>
         <li><a href="<?php echo get_category_link('4'); ?>">Law School</a></li>
-        <li><a href="<?php echo get_category_link('5'); ?>">MBA</a></li>
-        <li><a href="<?php echo get_category_link('7'); ?>">大学院/Ph.D</a></li>
+        <li><a href="<?php echo get_category_link('7'); ?>">MBA</a></li>
+        <li><a href="<?php echo get_category_link('5'); ?>">大学院/Ph.D</a></li>
         <li><a href="<?php echo get_category_link('6'); ?>">研究員</a></li>
         <li><a href="<?php echo get_category_link('8'); ?>">教授</a></li>
     </ul>
