0423803326a666bdc2306d32dfed7e250544b3b5
Yuka Suzuki <y-suzuki@fujisan.co.jp>
2015-01-21 22:10:33 -0800

    記者一覧のページを追加

--- wp-content/themes/wp.hatch.studio/contents/_authors.php
+++ wp-content/themes/wp.hatch.studio/contents/_authors.php
@@ -1,9 +1,10 @@
+<div id="author-list">
 
-<div class="title-icon"><span class="sub-title">Authors</span></div>
+    <div class="page-title">記者一覧</div>
 
-<ul class="Authors-list">
+    <ul>
 
-  <?php
+        <?php
 
   // 表示する順番にユーザーIDを設定する
 
@@ -11,24 +12,57 @@
 
   ?>
 
-  <?php foreach ($this_authors as $this_author_id ) { ?>
+        <?php foreach ($this_authors as $this_author_id ) { ?>
 
-    <li>
+        <li>
+            <a href="<?php echo get_author_posts_url($this_author_id)?>">
+                <img src="/wp-content/uploads/userphoto/<?php echo $this_author_id ?>.thumbnail.jpg">
+            </a>
 
-      <a href="<?php echo get_author_posts_url($this_author_id)?>"><img src="/wp-content/uploads/userphoto/<?php echo $this_author_id ?>.thumbnail.jpg"></a>
+            <dl>
 
-      <dl>
+                <dt>
+                    <a href="<?php echo get_author_posts_url($this_author_id)?>"><span
+                            class="name"><?php echo get_the_author_meta( 'first_name', $this_author_id ); ?> </span><span
+                            class="position">| キュレーター</span></a>
+                    since
+                    <?php
+                 $result =  $wpdb->get_var("
+                    SELECT post_date FROM hkm_posts
+                    WHERE post_type = 'post'
+                    AND post_status = 'publish'
+                    AND post_author = $this_author_id
+                    order by post_date
+                    limit 1
+                    ");
+                    $dtime = new DateTime($result);
+                    echo $dtime->format('Y-m-d');
+                    ?>
+                </dt>
 
-        <dt><a href="<?php echo get_author_posts_url($this_author_id)?>"><?php echo get_the_author_meta( 'first_name', $this_author_id ); ?></a></dt>
+                <dd>
 
-        <dd><a href="<?php echo get_author_posts_url($this_author_id)?>"><?php echo get_the_author_meta( 'description', $this_author_id ); ?></a></dd>
+                    <?php
+                 $result = (int) $wpdb->get_var("
+                    SELECT COUNT(*) FROM $wpdb->posts
+                    WHERE post_type = 'post'
+                    AND post_status = 'publish'
+                    AND post_author = $this_author_id
+                    ");
+                    echo $result;
+                    ?> post
 
-      </dl>
+                    <a href="<?php echo get_author_posts_url($this_author_id)?>"><?php echo get_the_author_meta( 'description', $this_author_id ); ?></a>
+                </dd>
 
-      <div class="clear-both"></div>
+            </dl>
 
-    </li>
+            <div class="clear-both"></div>
 
-  <?php } ?>
+        </li>
 
-</ul>
+        <?php } ?>
+
+    </ul>
+
+</div>
