71c8f38b099611bcdaa62282897801bac374bba3
Yuichi Ohneda <ohneda@gmail.com>
2015-01-25 21:07:11 -0800

    facebookのlikeボタンを、トップのカテゴリリンクの横に移動

--- wp-content/themes/wp.hatch.studio/components/_categories.php
+++ wp-content/themes/wp.hatch.studio/components/_categories.php
@@ -78,4 +78,24 @@
 
     </ul>
   </div>
+
+  <?php if (!is_user_logged_in()) : ?>
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+
+        var js, fjs = d.getElementsByTagName(s)[0];
+
+        if (d.getElementById(id)) return;
+
+        js = d.createElement(s); js.id = id;
+
+        js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1";
+
+        fjs.parentNode.insertBefore(js, fjs);
+
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+  <?php endif; ?>
+
 </div>
--- wp-content/themes/wp.hatch.studio/header.php
+++ wp-content/themes/wp.hatch.studio/header.php
@@ -147,26 +147,6 @@ if (has_post_thumbnail() && ! is_archive() && ! is_front_page() && ! is_home()){
 
 <body><!--- BODY -->
 
-<?php if (!is_user_logged_in()) : ?>
-
-<div id="fb-root"></div> 
-
-<script>(function(d, s, id) { 
-
-  var js, fjs = d.getElementsByTagName(s)[0]; 
-
-  if (d.getElementById(id)) return; 
-
-  js = d.createElement(s); js.id = id; 
-
-  js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1"; 
-
-  fjs.parentNode.insertBefore(js, fjs); 
-
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<?php endif; ?>
-
 <div id="container"><!-- CONTAINER -->
 
 <div id="contents"><!-- CONTENTS -->
