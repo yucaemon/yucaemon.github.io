e990e17e13dbd45c123422ed517987b36ec94a73
Yuka Suzuki <y-suzuki@fujisan.co.jp>
2015-02-11 22:24:47 -0800

    カテゴリーのヘッダーを取る。

--- wp-content/themes/wp.hatch.studio/contents/_index_header.php
+++ wp-content/themes/wp.hatch.studio/contents/_index_header.php
@@ -1,3 +1,11 @@
+
+<div class="title"><!-- TITLE -->
+    <h1>S<span class="first-a">a</span>n Fr<span
+            class="second-a">a</span>n<span class="c">c</span>isco</h1>
+
+    <h2>現地密着まがじん。</h2>
+</div>
+
 <div class="site-header">
   <p class="subtitle">はっちすたじおは、北カリフォルニアのバークレーを拠点にサンフラ、サンノゼ、シリコンバレーでやんちゃにくらす現地のやつらが適当につくる情報共有WEBまがじんです。</p>
 
@@ -7,42 +15,35 @@
 
   <p class="subtitle">にオープンしました。<br>北カリフォルニアの若者集まれ！</p>
 
-  <div class="event">
+    <div class="event">
 
-    <div class="coming-event">
+        <div class="coming-event">
 
-      <h3><span class="sub-title">イベント予定</span></h3>
+            <h3><span class="sub-title">イベント予定</span></h3>
 
-      <ul>
-        <li><a href="http://www.meetup.com/JapaneseMeetupBerkeley/events/191555892/">
-            <time>2014/未定</time>
-            <span>第5会目バークレー若者会開催予定！</span></a></li>
+            <ul>
+                <li><a href="http://www.meetup.com/JapaneseMeetupBerkeley/"><time>2014/2月</time> <span>バークレー若者会開催予定！</span></a></li>
+            </ul>
 
-      </ul>
+        </div>
+
+        <div class="future-event">
+
+            <h3> → 過去のイベント探検隊レボ</h3>
+
+            <ul>
+
+                <li><a href="http://www.meetup.com/JapaneseMeetup2014/"><time>2014/1/17</time> <span>Berkeley Meetup(英語)新年会開催しました！</span></a></li>
+
+                <li><a href="http://hatchstudioinc.com/archives/5292"><time>2014/11/15</time> <span>Berkeley Meetup(英語)開催しました！</span></a></li>
+
+                <li><a href="http://hatchstudioinc.com/archives/3780"><time>2014/08/23</time> 第5回、バークレー若者会しました！</a></li>
+
+
+            </ul>
+            <div class="past-event"><a href="http://hatchstudioinc.com/archives/category/meet"> → 過去のイベント</a></div>
+        </div>
 
     </div>
-
-    <div class="future-event">
-
-      <h3> → 過去のイベント探検隊レボ</h3>
-
-      <ul>
-
-        <li><a href="http://hatchstudioinc.com/archives/2589">
-            <time>2014/04/19</time>
-            第四回、バークレー若者会しました！</a></li>
-
-        <li><a href="http://www.meetup.com/JapaneseMeetup2014/">
-            <time>2014/07/07</time>
-            Berkeley Meetup(英語)しました！</a></li>
-
-        <li><a href="http://hatchstudioinc.com/?p=430">
-            <time>2014/04/27</time>
-            はっち会、寿司会しました！</a></li>
-
-      </ul>
-    </div>
-
-  </div>
   <!--END TITLE -->
 </div>
