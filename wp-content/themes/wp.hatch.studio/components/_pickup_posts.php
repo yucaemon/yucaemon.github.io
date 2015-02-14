ce8cd8322ddaa0394f04d22d84c8226ac621e1d6
Yuichi Ohneda <ohneda@gmail.com>
2015-02-14 01:51:11 -0800

    views数のトップ5を表示

---
+++ wp-content/themes/wp.hatch.studio/components/_pickup_posts.php
@@ -0,0 +1,44 @@
+<ul class="popular">
+
+  <div class="side-title">PICKUP POSTS</div>
+
+  <?php
+
+  $results = $wpdb->get_results("
+                    SELECT posts.ID, meta.meta_value FROM $wpdb->postmeta as meta
+                    join $wpdb->posts as posts on posts.ID = meta.post_id
+                    WHERE meta.meta_key = 'views'
+                    AND posts.post_status = 'publish'
+                    AND posts.post_author in (22,5,20,18,14,24,8,7,6,33,34,35,26,37,38,39,41,12)
+                    order by cast( meta.meta_value as signed) desc limit 5;
+                    ", ARRAY_N);
+
+  $average_views = array();
+
+  foreach ($results as $result) {
+    $average_views[$result[0]] = $result[1];
+  }
+
+  $view_ordered_post_ids = array_keys( $average_views )
+  ?>
+
+  <?php foreach ( array_slice( $view_ordered_post_ids, 0, 10 ) as $this_post_index => $this_post_id ) {
+    $this_post_views = $average_views[$this_post_id];
+    $this_post = get_post( $this_post_id );
+    ?>
+
+    <li>
+
+      <div class="popular-header">
+        <span class="name"><?php echo the_author_meta( 'nickname', $this_post->post_author ); ?> </span><span class="views"> - <?php echo $this_post_views ?>views</span>
+        <div class="popular-text">
+          <div class ="popular-title"><a href="<?php echo get_permalink($this_post->ID); ?>"><?php echo $this_post->post_title; ?></a></div>
+        </div>
+      </div>
+      <div class="popular-img"><a href="<?php echo get_permalink($this_post->ID); ?>"><?php echo get_the_post_thumbnail( $this_post->ID ); ?></a></div>
+
+    </li>
+
+  <?php } ?>
+
+</ul>
--- wp-content/themes/wp.hatch.studio/sidebar-2.php
+++ wp-content/themes/wp.hatch.studio/sidebar-2.php
@@ -1,4 +1,5 @@
 <div id="right-side"><!--RIGHT SIDE -->
   <?php include(TEMPLATEPATH.'/components/_popular_posts.php'); ?>
+  <?php include(TEMPLATEPATH.'/components/_pickup_posts.php'); ?>
   <?php include(TEMPLATEPATH.'/components/_socialMedia.php'); ?>
 </div>
