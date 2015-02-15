dc1268a096f8f859d9ab11005d5d646e492b2163
Yuichi Ohneda <ohneda@gmail.com>
2015-02-14 18:05:45 -0800

    カテゴリページのトップがiPhoneで正しく表示されるように修正

--- wp-content/themes/wp.hatch.studio/contents/_category_header.php
+++ wp-content/themes/wp.hatch.studio/contents/_category_header.php
@@ -2,21 +2,15 @@
 
 <?php $cat_id = $target_cat->term_id; ?>
 <div class="category-header">
-<div class="cate-img-top cook-img-top"><img
-    src="<?php echo get_template_directory_uri(); ?>/images/big_categories_img/T-<?php echo $cat_id ?>.png"></div>
+  <div class="cate-img-top">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/big_categories_img/T-<?php echo $cat_id ?>.png"></div>
+  <div class="cate-right-text">
+    <?php
+    $category = get_category($cat_id);
+    ?>
+    <h3><?php echo $category->slug ?></h3>
+    <p class="big-title"><?php echo $category->name ?></p>
+  </div>
 
-<div class="cate-right-text img-cook">
-  <?php
-
-  $category = get_category($cat_id);
-
-  ?>
-
-  <h3><?php echo $category->slug ?></h3>
-
-  <p class="big-title"><?php echo $category->name ?></p>
-
-</div>
-
-<div class="clear-both"></div>
+  <div class="clear-both"></div>
 </div>
