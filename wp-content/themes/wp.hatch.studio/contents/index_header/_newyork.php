65d88216887961300bf468962cdb71ae019d8d65
Yuca Suzuki <beru0891@yahoo.co.jp>
2015-02-23 21:02:50 -0800

    NY 版の色変更、レイアウトの直し

--- wp-content/themes/wp.hatch.studio/contents/index_header/_newyork.php
+++ wp-content/themes/wp.hatch.studio/contents/index_header/_newyork.php
@@ -1,10 +1,16 @@
-
 <h1>N<span>e</span>w Y<span>o</span><span>r</span>k</h1>
-<h2>現地密着まがじん。</h2>
-<p class="description">現地密着面白マガジン はっちすたじおは、海外各地、現地のみんなでつくる情報共有WEBまがじんです。</p>
-<div class="site-character left"><img src="<?php echo get_template_directory_uri(); ?>/images/top-newyork.png"></div>
+<h2><span>ニューヨーク</span><span>面白まがじん。</span></h2>
 
-<p class="date">03.01</p>
+<div class="description ny">
+    <p><span class="sub-title-top">NY</span>マンハッタン</p>
+    <h3>OPEN</h3>
+    <p>
+        ニューヨークに住んでみないとわからない現地情報を”Yuki”を中心に発信していきます〜♪
+    </p>
 
-<p class="description">にオープンしました。<br>おもしろ探検隊員、随時募集中！！</p>
+    <p class="date">03.01</p>
 
+    <p class="description">にオープンしました。<br>ニューヨーク在住の若者集まれ！</p>
+</div>
+
+<div class="site-character right"><img src="<?php echo get_template_directory_uri(); ?>/images/top-newyork.png"></div>
--- wp-content/themes/wp.hatch.studio/contents/index_header/_ucberkeley.php
+++ wp-content/themes/wp.hatch.studio/contents/index_header/_ucberkeley.php
@@ -1,13 +1,12 @@
-<h1>U<span>C</span> B<span>e</span>rkeley</h1>
-<h2><span>UCバークレー大学の</span><span>面白インタビュー</span></h2>
+<h1>U<span>c</span> <span>B</span><span>e</span>r<span>k</span><span>e</span><span>l</span><span>e</span><span>y</span></h1>
+<h2><span>UCバークレー大学の</span><span>面白まがじん。</span></h2>
 
 <div class="description ucb">
-  <p><span class="sub-title-top">UCB</span>UC バークレー大学の校内</p>
-  <h3>Interview</h3>
-  <p>
-    名門UCバークレー大学にいる人達はどんな人たちがいるの？という素朴な疑問にお答えして、現役UCバークレー生徒でもあり、自身も面白い経歴をもつ男”Takuya”を中心にUCバークレー大学にいる面白い人達をインタビューしていきます〜♪
-  </p>
+    <p><span class="sub-title-top">UCB</span>UC バークレー大学の校内</p>
+    <h3>Interview</h3>
+    <p>
+        名門UCバークレー大学にいる人達はどんな人たちがいるの？という素朴な疑問にお答えして、現役UCバークレー生徒でもあり、自身も面白い経歴をもつ男”Takuya”を中心にUCバークレー大学にいる面白い人達をインタビューしていきます〜♪
+    </p>
 </div>
 
 <div class="site-character right"><img src="<?php echo get_template_directory_uri(); ?>/images/top-ucb.png"></div>
-
