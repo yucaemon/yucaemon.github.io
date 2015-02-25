fc6842115370754c4283746dea0d08ab07ed134b
Yuichi Ohneda <ohneda@gmail.com>
2015-02-24 23:06:59 -0800

    カテゴリがページングされない不具合を修正

--- wp-content/themes/wp.hatch.studio/contents/_category_body.php
+++ wp-content/themes/wp.hatch.studio/contents/_category_body.php
@@ -1,2 +1,2 @@
-<?php query_posts("showposts=10&cat=$cat_id"); ?>
+<?php query_posts("showposts=12&cat=$cat_id&posts_per_page=1&paged=$paged"); ?>
 <?php include(TEMPLATEPATH . '/contents/_pickup.php'); ?>
