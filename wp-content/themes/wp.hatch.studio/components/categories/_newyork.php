1eec1f9c5a97d1a91e9008bea82a1428053a879d
Yuichi Ohneda <ohneda@gmail.com>
2015-03-11 21:07:34 -0700

    カテゴリの一覧のリンクを、実際のidに合わせて修正

--- wp-content/themes/wp.hatch.studio/components/categories/_newyork.php
+++ wp-content/themes/wp.hatch.studio/components/categories/_newyork.php
@@ -1,7 +1,7 @@
 <ul class="dropdown-menu" role="menu">
-  <li><a href="<?php echo get_category_link('2'); ?>">NY基礎知識</a></li>
-  <li><a href="<?php echo get_category_link('1'); ?>">アメリカ豆知識</a></li>
-  <li><a href="<?php echo get_category_link('5'); ?>">ビューティ</a></li>
-  <li><a href="<?php echo get_category_link('4'); ?>">留学紹介</a></li>
-  <li><a href="<?php echo get_category_link('3'); ?>">街の紹介</a></li>
+  <li><a href="<?php echo get_category_link('1'); ?>">NY基礎知識</a></li>
+  <li><a href="<?php echo get_category_link('2'); ?>">アメリカ豆知識</a></li>
+  <li><a href="<?php echo get_category_link('6'); ?>">ビューティ</a></li>
+  <li><a href="<?php echo get_category_link('7'); ?>">留学紹介</a></li>
+  <li><a href="<?php echo get_category_link('8'); ?>">街の紹介</a></li>
 </ul>
