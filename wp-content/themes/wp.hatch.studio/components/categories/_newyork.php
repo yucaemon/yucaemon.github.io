f71e9166d8b2689c4f9c0b6f6b8534a2d4fc8c7d
Yuichi Ohneda <ohneda@gmail.com>
2015-03-18 22:40:33 -0700

    newyorkのカテゴリを修正

--- wp-content/themes/wp.hatch.studio/components/categories/_newyork.php
+++ wp-content/themes/wp.hatch.studio/components/categories/_newyork.php
@@ -1,7 +1,10 @@
 <ul class="dropdown-menu" role="menu">
   <li><a href="<?php echo get_category_link('1'); ?>">NY基礎知識</a></li>
+  <li><a href="<?php echo get_category_link('22'); ?>">Cafe</a></li>
+  <li><a href="<?php echo get_category_link('18'); ?>">Fashion</a></li>
+  <li><a href="<?php echo get_category_link('12'); ?>">Foods</a></li>
   <li><a href="<?php echo get_category_link('2'); ?>">アメリカ豆知識</a></li>
   <li><a href="<?php echo get_category_link('6'); ?>">ビューティ</a></li>
-  <li><a href="<?php echo get_category_link('7'); ?>">留学紹介</a></li>
+  <li><a href="<?php echo get_category_link('7'); ?>">Study in New York</a></li>
   <li><a href="<?php echo get_category_link('8'); ?>">街の紹介</a></li>
 </ul>
