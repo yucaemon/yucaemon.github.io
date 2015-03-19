29618d7d5cdac7229bc58325582f8c28de765207
Yuca Suzuki <beru0891@yahoo.co.jp>
2015-03-18 22:59:59 -0700

    NYインデックスのトップのメンバー告知を消す

--- wp-content/themes/wp.hatch.studio/contents/index_header/_newyork.php
+++ wp-content/themes/wp.hatch.studio/contents/index_header/_newyork.php
@@ -7,16 +7,7 @@
 </span>
 </p>
 <div class="site-character left"><img src="<?php echo get_template_directory_uri(); ?>/images/top-newyork.png"></div>
-<p class="date">03.22</p>
+<p class="date">03.24</p>
 <p class="description second-description">にオープンしました。<br>ニューヨークの若者集まれ！</p>
-
-<div class="notice">
-
-    <p class="sub-title"> NY在住のライター仲間募集中</p>
-
-    <p>興味のある方は、facebookからお問い合わせ下さい。</p>
-    <div class="past-event"><a href="http://hatchstudioinc.com/archives/category/meet"> → こちら</a></div>
-</div>
-
 </div>
 
