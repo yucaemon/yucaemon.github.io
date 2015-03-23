104596cfe9e4afa218971e80d913a36e91b27ce2
Yuca Suzuki <beru0891@yahoo.co.jp>
2015-03-22 21:43:30 -0700

    UCBのindexヘッダーのレイアウトを変更、統一させた

--- wp-content/themes/wp.hatch.studio/contents/index_header/_ucberkeley.php
+++ wp-content/themes/wp.hatch.studio/contents/index_header/_ucberkeley.php
@@ -1,11 +1,12 @@
 <h1>U<span>C</span> <span>B</span><span>e</span>r<span>k</span><span>e</span><span>l</span><span>e</span><span>y</span></h1>
-<h2><span>UCバークレー大学の</span><span>面白まがじん。</span></h2>
-<div class="site-character left"><img src="<?php echo get_template_directory_uri(); ?>/images/top-ucb.png"></div>
-<div class="description">
-    <p><span class="sub-title-top">UCB</span>UC バークレー大学の校内</p>
-    <h3>Interview</h3>
-    <p>
-        名門UCバークレー大学にいる人達はどんな人たちがいるの？という素朴な疑問にお答えして、現役UCバークレー生徒でもあり、自身も面白い経歴をもつ男”Takuya”を中心にUCバークレー大学にいる面白い人達をインタビューしていきます〜♪
-    </p>
-</div>
+<h2>大学密着まがじん。</h2>
 
+<p class="description">
+<span class="description-top-text">
+ 名門UCバークレー大学にいる人達はどんな人たちがいるの？という素朴な疑問にお答えして、現役UCバークレー生徒でもあり、自身も面白い経歴をもつ男”Takuya”を中心にUCバークレー大学にいる面白い人達をインタビューしていきます〜♪
+</span>
+</p>
+<div class="site-character left"><img src="<?php echo get_template_directory_uri(); ?>/images/top-ucb.png"></div>
+<p class="date">03.24</p>
+<p class="description second-description">にオープンしました。<br>UCバークレー大学生徒達集まれ！</p>
+</div>
