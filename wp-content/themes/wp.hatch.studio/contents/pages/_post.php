f0df6398d7782ee483a15eb021e014af141e0bbd
Yuca Suzuki <beru0891@yahoo.co.jp>
2015-07-20 22:23:29 -0700

    記事ページのレイアウトの修正（未完成）

--- wp-content/themes/wp.hatch.studio/contents/pages/_post.php
+++ wp-content/themes/wp.hatch.studio/contents/pages/_post.php
@@ -1,66 +1,56 @@
 
 <div class="post">
 
-  <?php while (have_posts()) : the_post(); ?>
+    <?php while (have_posts()) : the_post(); ?>
 
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-      <header class="entry-header">
+    <header class="entry-header">
 
         <?php if (!post_password_required()) : ?>
 
-          <div class="mini-info">
-
-            <span class="sub-title"><?php echo time_ago(); ?></span>
-
-                    <span class="views"><?php if (function_exists('the_views')) {
-                        the_views();
-                      } ?></span>
-
-            <span class="category-name">カテゴリ：<?php the_category(', '); ?></span>
-
-          </div>
 
 
-          <h1 class="entry-title"> <a title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php echo mb_substr($post->post_title, 0, 67).''; ?></a></h1>
+        <h1 class="entry-title"> <a title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php echo mb_substr($post->post_title, 0, 67).''; ?></a></h1>
 
 
 
-          <div class="api-share-btns">
+
+        <div class="api-share-btns">
 
             <?php if (!is_user_logged_in()) : ?>
 
-              <div class="facebook-like-btn">
+            <div class="facebook-like-btn">
 
                 <iframe src="//www.facebook.com/plugins/like.php?href=<?php the_permalink() ?>&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=650457541700749"
                         scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;"
                         allowTransparency="true"></iframe>
 
-              </div>
+            </div>
 
             <?php endif; ?>
 
             <div class="twitter-share-botn">
 
-              <a href="https://twitter.com/share" class="twitter-share-button"
-                 data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" 　
-                 data-via="hatchstudioinc" data-lang="ja" data-hashtags="海外おもしろまがじん" data-dnt="true">ツイート</a>
+                <a href="https://twitter.com/share" class="twitter-share-button"
+                   data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" 　
+                data-via="hatchstudioinc" data-lang="ja" data-hashtags="海外おもしろまがじん" data-dnt="true">ツイート</a>
 
-              <script>!function (d, s, id) {
-                  var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-                  if (!d.getElementById(id)) {
-                    js = d.createElement(s);
-                    js.id = id;
-                    js.src = p + '://platform.twitter.com/widgets.js';
-                    fjs.parentNode.insertBefore(js, fjs);
-                  }
+                <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + '://platform.twitter.com/widgets.js';
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
                 }(document, 'script', 'twitter-wjs');</script>
 
             </div>
 
-          </div>
+        </div>
 
-          <div class="entry-thumbnail">
+        <div class="entry-thumbnail">
 
             <?php
 
@@ -72,8 +62,8 @@
 
               ?>
 
-              <img src="<?php echo get_template_directory_uri(); ?>/images/NoImg.png"
-                   class="attachment-post-thumbnail">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/NoImg.png"
+                 class="attachment-post-thumbnail">
 
             <?php
 
@@ -81,58 +71,50 @@
 
             ?>
 
-          </div>
+        </div>
 
-          <div class="authors-info">
-            <div class="pickup-authors">
-              <a href="<?php echo get_author_posts_url(get_the_author_ID()) ?>">
-                <?php userphoto_the_author_photo() ?>
-              </a>
+        <div class="sub-text-content">
+            <div class="author-photo">
+                <a href="<?php echo get_author_posts_url(get_the_author_ID()) ?>">
+                    <?php userphoto_the_author_photo() ?>
+                </a>
             </div>
+                <p class="author-nickname"><?php the_author_nickname(); ?></p>
+            <span class="time"><i class="fa fa-clock-o"></i><?php echo the_date(" M.d.Y - h:s a"); ?></span>
 
-            <p class="name"><?php the_author_nickname(); ?></p>
 
-            <p class="author-about"><?php the_author_description(); ?></p>
-
-          </div>
+        </div>
 
         <?php endif; ?>
 
-      </header>
-      <!-- .entry-header -->
+    </header>
+    <!-- .entry-header -->
 
-      <div class="entry-content">
+    <div class="entry-content">
 
         <?php the_content(); ?>
 
-      </div>
-      <!-- .entry-content -->
+    </div>
+    <!-- .entry-content -->
 
     </article><!-- #post -->
 
-  <?php endwhile; ?>
+    <?php endwhile; ?>
 
-    <div class="navi-pager">
-
-        <div class="pre"><?php previous_post_link('%link', '前の記事へ', false, 22) ?></div>
-
-        <?php $next_post_link = get_next_post_link('%link', '次の記事へ', false, 22); ?>
-
-        <?php if ($next_post_link) : ?>
-
-        <div class="next"><?php echo $next_post_link ?></div>
-
-        <?php endif; ?>
-
+    <div class="authors-content">
+        <div class="author-photo">
+            <a href="<?php echo get_author_posts_url(get_the_author_ID()) ?>">
+                <?php userphoto_the_author_photo() ?>
+            </a>
+        </div>
+        <div class="author-excerpt">
+            <p class="author-nickname"><?php the_author_nickname(); ?></p>
+            <p class="author-description"><?php the_author_description(); ?></p>
+        </div>
     </div>
 
+
 </div>
-
-<?php if (!is_user_logged_in()) : ?>
-
-  <!--div class="fb-comments" data-href="<?php the_permalink(); ?>" data-num-posts="10"></div-->
-
-<?php endif; ?>
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- はっち広告ーレスポンシブ -->
@@ -142,7 +124,7 @@
      data-ad-slot="7838417156"
      data-ad-format="auto"></ins>
 <script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
+    (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 
 <div class="post-related"><?php get_yuzo_related_posts(); ?></div>
