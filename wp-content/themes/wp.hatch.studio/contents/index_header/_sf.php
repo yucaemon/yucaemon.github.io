5d673256ce19f4cf336468540cb5297a3294b88e
Yuca Suzuki <beru0891@yahoo.co.jp>
2015-08-21 03:31:53 -0700

    携帯でのレイアウト崩れ修正

--- wp-content/themes/wp.hatch.studio/contents/index_header/_sf.php
+++ wp-content/themes/wp.hatch.studio/contents/index_header/_sf.php
@@ -1,35 +1,21 @@
 <h1>S<span class="first-a">a</span>n Fr<span
     class="second-a">a</span>n<span class="c">c</span>isco</h1>
-<h2>現地密着まがじん。</h2>
 
-<p class="description">はっちすたじおは、北カリフォルニアのバークレーを拠点にサンフラ、サンノゼ、シリコンバレーでやんちゃにくらす現地のやつらが適当につくる情報共有WEBまがじんです。</p>
-<div class="site-character left"><img src="<?php echo get_template_directory_uri(); ?>/images/top-sf-3rd.png"></div>
+<div class="eye-catch">
+  <h2>現地密着まがじん。</h2>
+  <div class="site-character"><img src="<?php echo get_template_directory_uri(); ?>/images/top-sf-3rd.png"></div>
+</div>
 
-<p class="date">Editors wanted !!!</p>
+<div class="description">はっちすたじおは、北カリフォルニアのバークレーを拠点にサンフラ、サンノゼ、シリコンバレーでやんちゃにくらす現地のやつらが適当につくる情報共有WEBまがじんです。</p>
+  <p class="date">Editors wanted !!!</p>
+  <p class="description">06.28.2014 開設しました。<br>北カリフォルニアの若者集まれ！</p>
 
-<p class="description">06.28.2014 開設しました。<br>北カリフォルニアの若者集まれ！</p>
 
 <div class="event">
-
     <ul class="important-notice">
         <li><a href="http://newyork.hatchstudioinc.com/">エリア拡大！はっちすたじおニューヨーク版公開！</a></li>
-        <li><a href="http://newyork.hatchstudioinc.com/">ニューヨーク版　　</a><a href="http://ucberkeley.hatchstudioinc.com/">UCバークレー大学版</a></li>
+        <li><a href="http://newyork.hatchstudioinc.com/">ニューヨーク版</a><a href="http://ucberkeley.hatchstudioinc.com/">UCバークレー大学版</a></li>
     </ul>
-
-
-  <div class="future-event">
-
-    <p class="sub-title"> → 過去のイベント探検隊レボ</p>
-
-    <ul>
-      <li><a href="http://www.meetup.com/JapaneseMeetupBerkeley/events/220510792/"><time>2015/3/21</time> <span>第6回バークレー若者会開催しました！</span></a>
-
-      <li><a href="http://www.meetup.com/JapaneseMeetup2014/"><time>2014/1/17</time> <span>Berkeley Meetup(英語)新年会開催しました！</span></a></li>
-
-      <li><a href="http://hatchstudioinc.com/archives/5292"><time>2014/11/15</time> <span>Berkeley Meetup(英語)開催しました！</span></a></li>
-
-    </ul>
-    <div class="past-event"><a href="http://hatchstudioinc.com/archives/category/meet"> → 過去のイベント</a></div>
-  </div>
+</div>
 
 </div>
