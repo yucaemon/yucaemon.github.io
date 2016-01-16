8e281055aa80095fbe99a871cc3e3bdfffd1b72f
Yuca Suzuki <beru0891@yahoo.co.jp>
2016-01-15 22:07:56 -0800

    UCLAのデザイン反映

--- wp-content/themes/new.wp.hatch.studio/contents/index/_index_header.php
+++ wp-content/themes/new.wp.hatch.studio/contents/index/_index_header.php
@@ -1,1 +1,1 @@
-<?php include(TEMPLATEPATH . '/contents/' . $content_type . '_header/_sf.php'); ?>
+<?php include(TEMPLATEPATH . '/contents/' . $content_type . '_header/_' . $site_name[get_current_blog_id()] . '.php'); ?>
