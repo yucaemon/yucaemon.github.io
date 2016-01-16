e8a5d8c59b98814e1435ad647d911135034997d4
Yuca Suzuki <beru0891@yahoo.co.jp>
2016-01-16 13:40:54 -0800

    imageの整理とアバウトのキャラ画像をつけた

--- wp-content/themes/new.wp.hatch.studio/components/about/_ucla.php
+++ wp-content/themes/new.wp.hatch.studio/components/about/_ucla.php
@@ -1,16 +1,12 @@
 <div class ="about">
-
-    <div class="about-logos">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/ucb/logo-ucb.png">
-
+    <div class="about-icon">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/ucla/about/about-icon-ucla.png">
     </div>
-
     <div class="description">
         はっちすたじおUCLAはロサンゼルス周辺にいる日本人で協力して、「スキマ時間にサクッとクスッと何かを得られる記事」をお届けしたいと思い作りました。大学で学んだことや、アメリカ生活を通して発見したことなど、どなたにも楽しく読んで何かの役に立てて頂けるような記事を発信していけるように努めます。この活動を通して、ロサンゼルスでの素敵な日本人コミュニティー作りにも貢献出来たら嬉しいです。
     </div>
-
     <div class="about-signs">
-        <div class="sign"><img src="<?php echo get_template_directory_uri(); ?>/images/ucla/ucla-sign.png"></div>
+        <div class="sign"><img src="<?php echo get_template_directory_uri(); ?>/images/ucla/about/ucla-sign.png"></div>
         <p class="created-date">1/18/2016</p>
     </div>
 
